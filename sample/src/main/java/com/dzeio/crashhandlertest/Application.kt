64d9b25883a9423f749df1bb6bd36080c46e6fd5
Avior <f.bouillon@aptatio.com>
2023-03-17 17:27:01 +0100

    feat: simplified sample app code to better understand how it work (#10)

--- sample/src/main/java/com/dzeio/crashhandlertest/Application.kt
+++ sample/src/main/java/com/dzeio/crashhandlertest/Application.kt
@@ -8,15 +8,25 @@ class Application : android.app.Application() {
     override fun onCreate() {
         super.onCreate()
 
+        // get the device Preference store
         val prefs = PreferenceManager.getDefaultSharedPreferences(this)
 
+        // create the Crash Handler
         CrashHandler.Builder()
-            .withActivity(ErrorActivity::class.java)
+            // need the application context to run
             .withContext(this)
+            // define a custom activity to use
+            .withActivity(ErrorActivity::class.java)
+            // define the preferenceManager to be able to handle crash in a custom Activity and to have the previous crash date in the logs
             .withPrefs(prefs)
             .withPrefsKey("com.dzeio.crashhandler.key")
-            .withPrefix("Pouet :D")
+            // a Prefix to add at the beginning the crash message
+            .withPrefix(
+                "POKEMON"
+            )
+            // a Suffix to add at the end of the crash message
             .withSuffix("WHYYYYY")
+            // build & start the module
             .build().setup()
     }
 }
