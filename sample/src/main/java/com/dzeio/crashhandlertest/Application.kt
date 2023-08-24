acbb524a7ee4bf4fb4b85d5316756041568091df
Avior <f.bouillon@aptatio.com>
2023-08-24 15:54:55 +0200

    feat: Add crash saving (#12)

--- sample/src/main/java/com/dzeio/crashhandlertest/Application.kt
+++ sample/src/main/java/com/dzeio/crashhandlertest/Application.kt
@@ -3,6 +3,7 @@ package com.dzeio.crashhandlertest
 import androidx.preference.PreferenceManager
 import com.dzeio.crashhandler.CrashHandler
 import com.dzeio.crashhandlertest.ui.ErrorActivity
+import java.io.File
 
 class Application : android.app.Application() {
     override fun onCreate() {
@@ -15,17 +16,20 @@ class Application : android.app.Application() {
         CrashHandler.Builder()
             // need the application context to run
             .withContext(this)
+            // every other items below are optionnal
             // define a custom activity to use
             .withActivity(ErrorActivity::class.java)
-            // define the preferenceManager to be able to handle crash in a custom Activity and to have the previous crash date in the logs
+            // define the preferenceManager to have the previous crash date in the logs
             .withPrefs(prefs)
             .withPrefsKey("com.dzeio.crashhandler.key")
             // a Prefix to add at the beginning the crash message
-            .withPrefix(
-                "Pokémon"
-            )
+            .withPrefix("Prefix")
             // a Suffix to add at the end of the crash message
             .withSuffix("Suffix")
+            // add a location where the crash logs are also exported (can be recovered as a zip ByteArray by calling {CrashHandler.getInstance().export()})
+            .withExportLocation(
+                File(this.getExternalFilesDir(null) ?: this.filesDir, "crash-logs")
+            )
             // build & start the module
             .build().setup()
     }
--- sample/src/main/java/com/dzeio/crashhandlertest/ui/MainActivity.kt
+++ sample/src/main/java/com/dzeio/crashhandlertest/ui/MainActivity.kt
@@ -1,15 +1,40 @@
 package com.dzeio.crashhandlertest.ui
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
+import com.dzeio.crashhandler.CrashHandler
 import com.dzeio.crashhandlertest.R
 import com.dzeio.crashhandlertest.databinding.ActivityMainBinding
 
-/**
- *
- */
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * Response to the export button event
+     */
+    private val writeResult = registerForActivityResult(
+        ActivityResultContracts.CreateDocument("application/zip")
+    ) {
+
+        val zipFile = CrashHandler.getInstance().export()
+        if (zipFile == null) {
+            return@registerForActivityResult
+        }
+
+        // write file to location
+        this.contentResolver.openOutputStream(it!!)?.apply {
+            write(zipFile)
+            close()
+        }
+
+        Toast.makeText(
+            this,
+            R.string.export_complete,
+            Toast.LENGTH_LONG
+        ).show()
+    }
 
     private lateinit var binding: ActivityMainBinding
 
@@ -23,6 +48,16 @@ class MainActivity : AppCompatActivity() {
         binding.buttonFirst.setOnClickListener {
             // DIE
             throw Exception(getString(R.string.error_message))
+        }
+
+        binding.buttonExport.setOnClickListener {
+            // launch the popin to select where to save the file
+            writeResult.launch("output.zip")
+        }
+
+        binding.buttonClearExports.setOnClickListener {
+            // clear the handler exports
+            CrashHandler.getInstance().clearExports()
         }
     }
 }
