8ef9bc8ee8ad055e589e8ded80052dff9bb6fa7e
Avior <f.bouillon@aptatio.com>
2026-03-10 16:38:11 +0100

    Implement Git command execution for versioning
    Add functionality to retrieve Git metadata for versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,81 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.7.0" apply false
 }
 
+// from: https://discuss.kotlinlang.org/t/use-git-hash-as-version-number-in-build-gradle-kts/19818/8
+fun String.runCommand(
+    workingDir: File = File("."),
+    timeoutAmount: Long = 60,
+    timeoutUnit: TimeUnit = TimeUnit.SECONDS
+): String = ProcessBuilder(split("\\s(?=(?:[^'\"`]*(['\"`])[^'\"`]*\\1)*[^'\"`]*$)".toRegex()))
+    .directory(workingDir)
+    .redirectOutput(ProcessBuilder.Redirect.PIPE)
+    .redirectError(ProcessBuilder.Redirect.PIPE)
+    .start()
+    .apply { waitFor(timeoutAmount, timeoutUnit) }
+    .run {
+        val error = errorStream.bufferedReader().readText().trim()
+        if (error.isNotEmpty()) {
+            return@run ""
+        }
+        inputStream.bufferedReader().readText().trim()
+    }
+
+val branch = "git rev-parse --abbrev-ref HEAD".runCommand(workingDir = rootDir)
+val tag = "git tag -l --points-at HEAD".runCommand(workingDir = rootDir)
+val commitId = "git rev-parse HEAD".runCommand(workingDir = rootDir)
+
+val finalVersion = System.getenv("version") ?: tag.drop(1) ?: "1.0.0"
+val finalGroup = System.getenv("group") ?: "com.dzeio"
+val artifact = System.getenv("artifact") ?: "crashhandler"
+
+group = finalGroup
+version = finalVersion
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = finalGroup
+            artifactId = artifact
+            version = finalVersion
+
+            from(components["java"])
+
+            pom {
+                name.set("TCGdex SDK")
+                description.set("Communicate with the Open Source TCGdex API in Kotlin/Java using the SDK")
+                url.set("https://github.com/dzeiocom/crashhandler")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://github.com/dzeiocom/crashhandler/blob/master/LICENSE")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("avior")
+                        name.set("Avior")
+                        email.set("contact@dze.io")
+                    }
+                }
+                scm {
+                    connection.set("scm:git@github.com:dzeiocom/crashhandler.git")
+                    url.set("https://github.com/dzeiocom/crashhandler")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "GithubPackages"
+            url = uri("https://maven.pkg.github.com/dzeiocom/crashhandler")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+}
+
 task("clean") {
     delete(rootProject.buildDir)
     delete(project.buildDir)
